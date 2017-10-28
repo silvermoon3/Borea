4da4121e3c82e4443dbf7af7edc3a14aec37296d
silvermoon3 <silvermoon3@users.noreply.github.com>
2017-10-28 12:47:33 -0400

    covoiturage, version 1

---
+++ app/src/main/java/uqac/eslie/nova/Helper/TimePickerFragment.java
@@ -0,0 +1,39 @@
+package uqac.eslie.nova.Helper;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import uqac.eslie.nova.BDD.CarPooling;
+import uqac.eslie.nova.LoginActivity;
+import uqac.eslie.nova.addCarPooling;
+
+/**
+ * Created by ESTEL on 27/10/2017.
+ */
+
+public  class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
--- app/src/main/java/uqac/eslie/nova/addCarPooling.java
+++ app/src/main/java/uqac/eslie/nova/addCarPooling.java
@@ -1,32 +1,47 @@
 package uqac.eslie.nova;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+
+import android.widget.DatePicker;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
+
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 import uqac.eslie.nova.BDD.CarPooling;
 import uqac.eslie.nova.Fragments.AddCarpoolingFragment;
-import uqac.eslie.nova.Fragments.HomeFragment;
+import uqac.eslie.nova.Helper.DatePickerFragment;
+
 
 public class addCarPooling extends AppCompatActivity {
 
-    private TextView address;
+    private TextView addressD;
+    private TextView addressA;
+    private TextView time;
+    private TextView date;
+    private TextView hourD;
+    private TextView hourR;
+    private TextView price;
+    private TextView place;
 
 
 
@@ -57,20 +72,30 @@ public class addCarPooling extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case R.id.menu_item_valid:
-                address =  findViewById(R.id.adress_carPooling);
-                //On ajoute le covoiturage
-                CarPooling carPooling = new CarPooling();
-                carPooling.setPrice(2.0);
-                carPooling.setDepart(address.toString());
+                addressD = findViewById(R.id.addressD_carPooling);
+                addressA = findViewById(R.id.addressA_carPooling);
+                price = findViewById(R.id.price_carPooling);
+                date = findViewById(R.id.date_carPooling);
 
-                // insert the new item into the database
-                FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-                DatabaseReference mReference = mDatabase.getReference("CarPooling");
-                String ID = mReference.push().getKey();
-                mReference.child(ID).setValue(carPooling);
+                if (date.getText() != null && price.getText() != null)
+                {
+                    //On ajoute le covoiturage
 
-                // add item
-                break;
+                    CarPooling carPooling = new CarPooling();
+                    carPooling.setDepart(addressD.getText().toString());
+                    carPooling.setDestination(addressA.getText().toString());
+                    carPooling.setPrice(Double.parseDouble(price.getText().toString()));
+                    carPooling.setDate(date.getText().toString());
+
+                    // insert the new item into the database
+                    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+                    DatabaseReference mReference = mDatabase.getReference("CarPooling");
+                    String ID = mReference.push().getKey();
+                    mReference.child(ID).setValue(carPooling);
+
+                    // add item
+                    break;
+                }
 
 
 
@@ -78,6 +103,36 @@ public class addCarPooling extends AppCompatActivity {
 
         }
         return true;
+    }
+
+    public void showTimePickerDialog(View v) {
+        Calendar currentTime = Calendar.getInstance();
+        int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = currentTime.get(Calendar.MINUTE);
+        TimePickerDialog newFragment = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute ) {
+                time =  findViewById(R.id.hour_carPooling);
+                time.setText(hour +":" +minute);
+            }
+        }, hour, minute, true);
+
+        newFragment.show();
+    }
+
+    public void showDatePickerDialog(View v) {
+        Calendar currentDate = Calendar.getInstance();
+        int year = currentDate.get(Calendar.YEAR);
+        int month = currentDate.get(Calendar.MONTH);
+        int day = currentDate.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog newFragment = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                date =  findViewById(R.id.date_carPooling);
+                date.setText(day +"/" +month+"/"+year);
+            }
+        }, year, month, day);
+        newFragment.show();
     }
 
 
